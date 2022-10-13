2fc45ab497dc3f5347ed07ad4f37f169e58cb42f
SamuelPrigent <80969347+SamuelPrigent@users.noreply.github.com>
2022-10-13 16:40:06 +0200

    recipes section is now responsive

--- javascript/pages/index.js
+++ javascript/pages/index.js
@@ -1,12 +1,29 @@
-// Display data
-console.log(recipes);
+// ====== Display data ======
+// via console.log(recipes);
 
 // Dom elements
 const recipesSection = document.querySelector(".section-recipes");
 
-// for Each Recipe => Create Card
+// forEach Recipe => Create Card via a Factory Patern (getRecipeCard)
 recipes.forEach((recipes) => {
   const newRecipeCard = getRecipeCard(recipes);
   recipesSection.appendChild(newRecipeCard);
-  console.log(recipes.name);
 });
+
+// ====== Filtres ======
+
+// Listes des éléments des boutons Filter Search - Ingrédients
+console.log(recipes);
+
+// Filtre Array
+const set = new Set(recipes);
+
+const ustensilsValues = recipes.map((e) => e.ustensils);
+console.log(ustensilsValues);
+
+const setUstensils = new Set(ustensilsValues);
+// console.log(setUstensils);
+
+// Filtre par Appareil - button value // (Element qui ne contiennent QUE Saladier)
+const apparel = recipes.filter((element) => element.appliance == "Saladier");
+// console.log(apparel);
