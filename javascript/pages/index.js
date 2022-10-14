1b27774358baf1215f52c0d520facd41944970e5
SamuelPrigent <80969347+SamuelPrigent@users.noreply.github.com>
2022-10-14 12:00:36 +0200

    data list for button

--- javascript/pages/index.js
+++ javascript/pages/index.js
@@ -1,5 +1,4 @@
-// ====== Display data ======
-// via console.log(recipes);
+// ====== Display data (recipes) ======
 
 // Dom elements
 const recipesSection = document.querySelector(".section-recipes");
@@ -12,18 +11,37 @@ recipes.forEach((recipes) => {
 
 // ====== Filtres ======
 
-// Listes des éléments des boutons Filter Search - Ingrédients
-console.log(recipes);
+// Listes : [ Ingrédients - Ustensiles - Appareil ]
 
-// Filtre Array
-const set = new Set(recipes);
+// === Listes des Ingrédients (IngredientsList) ===
+const IngredientsCombinaison = recipes.map((e) => e.ingredients); // all combinaison (recipes)
+const IngredientsValues = IngredientsCombinaison.flat(1); // all ingredients array with qty
+const IngredientOnlyArray = IngredientsValues.map((e) => e.ingredient); // all ingredient name with double
+const IngredientOnlyArrayLowerCase = IngredientOnlyArray.map((element) => {
+  return element.toLowerCase();
+});
+const IngredientsList = new Set(IngredientOnlyArrayLowerCase); // list without double (sauf err majuscules)
+const newIngredientsList = Array.from(IngredientsList);
+newIngredientsList.sort((a, b) => (a > b ? 1 : -1));
+// console.log(newIngredientsList);
 
-const ustensilsValues = recipes.map((e) => e.ustensils);
-console.log(ustensilsValues);
+// === Listes des Ustensils (UstensilsList) ===
+const UstensilsCombinaison = recipes.map((e) => e.ustensils);
+const UstensilsValues = UstensilsCombinaison.flat(1); // all values with double
+const UstensilsList = new Set(UstensilsValues); // list without double
+// console.log(UstensilsList);
 
-const setUstensils = new Set(ustensilsValues);
-// console.log(setUstensils);
+// === Liste des Appareils (ApparelList) ===
+const ApparelCombinaison = recipes.map((e) => e.appliance);
+const ApparelValues = ApparelCombinaison.flat(1); // all values with double
+const ApparelList = new Set(ApparelValues); // list without double
+// console.log(ApparelList);
 
-// Filtre par Appareil - button value // (Element qui ne contiennent QUE Saladier)
+//
+//
+//
+// ======== Filtre par Appareil - button value  ========
+
+// (Element qui ne contiennent QUE Saladier)
 const apparel = recipes.filter((element) => element.appliance == "Saladier");
 // console.log(apparel);
