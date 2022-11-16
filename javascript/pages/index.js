03b8e3408cbba3c98a6d8c4fb8533833dfc33c18
SamuelPrigent <80969347+SamuelPrigent@users.noreply.github.com>
2022-11-16 17:47:10 +0100

    last

--- javascript/pages/index.js
+++ javascript/pages/index.js
@@ -256,7 +256,7 @@ AllSearchBar.forEach((element) => {
 //
 // ===== Actions de Recherche par Type ======
 
-// ** master branch with forEach and Array push **
+// ** master  with forEach + Array push **
 
 // === Première Recherche ===
 
